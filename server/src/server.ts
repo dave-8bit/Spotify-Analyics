6612c993ea16ead071df9901b0b67f9303188dc3
dave-8bit <chinwenmerid@gmail.com>
2026-03-28 13:54:45 +0100

    Server and database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app";
+import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
